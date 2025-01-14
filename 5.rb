7219dd4f7673237e3b805d7a794ab708d1803654
Laura Nunes <lauranunes2508@gmail.com>
2025-01-14 19:41:48 -0300

    Dinâmica de POO entre classes

---
+++ 5.rb
@@ -0,0 +1,34 @@
+# Criar 3 classes que conversem entre si
+# Quando as classes estão em arquivos separados, é necessário usar require_relative 'nome_do_arquivo'
+
+
+class Produto
+
+    attr_accessor :name, :price
+
+    def initialize(name, price)
+        @name = name
+        @price = price
+    end
+end
+
+
+class Mercado
+
+    def initialize (produto)
+        @produto = produto
+    end
+
+    def comprar
+        puts "Você comprou o produto #{@produto.name} no valor de R$#{@produto.price}"
+    end
+end
+
+class Final
+
+    produto = Produto.new("Fogão", 800.0)
+    mercado = Mercado.new(produto)
+
+    mercado.comprar
+
+end
